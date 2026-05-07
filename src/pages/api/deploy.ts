9dfdbc8abb588eb3f9b163720be224851e5890aa
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-05-07 22:21:11 +0530

    Delete src/pages/api directory

--- src/pages/api/deploy.ts
+++
@@ -1,7 +0,0 @@
-import { deploySite } from "@/lib/deploy/netlify";
-
-export default async function handler(req, res) {
-  const deployed = await deploySite(req.body.files);
-
-  res.json(deployed);
-}
--- src/pages/api/preview.ts
+++
@@ -1,7 +0,0 @@
-import { createPreview } from "@/lib/sandbox";
-
-export default async function handler(req, res) {
-  const url = await createPreview(req.body.files);
-
-  res.json(url);
-}
