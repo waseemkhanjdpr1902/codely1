238d015c7cb71bda93e33544332ae791c5e52209
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-05-07 14:27:39 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 
 export default function Home() {
-  return (<><Navbar /><Hero /></>);
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
