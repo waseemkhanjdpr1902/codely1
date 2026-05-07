b7d7e5f6a1fca79720c5c0692450a212baf9732c
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-05-07 22:34:28 +0530

    Update zip.ts

--- src/lib/zip.ts
+++ src/lib/zip.ts
@@ -1,13 +1,5 @@
-import JSZip from "jszip";
+export async function exportZip(files: any[]) {
+  console.log("ZIP export placeholder", files);
 
-export async function exportZip(files) {
-  const zip = new JSZip();
-
-  files.forEach((f) => {
-    zip.file(f.path, f.content);
-  });
-
-  return await zip.generateAsync({
-    type: "blob"
-  });
+  return null;
 }
