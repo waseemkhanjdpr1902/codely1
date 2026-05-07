9cc15a8f8c9fe3eee6c56a571fc10cd37b3aaefc
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-05-07 16:00:08 +0530

    Update admin.ts

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+
+const serviceKey =
+  process.env.SUPABASE_SERVICE_ROLE_KEY || "";
+
 export const admin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  supabaseUrl,
+  serviceKey
 );
